0c8a01026aebbe74a984891f911adbc5de25c198
M-FF-M <fabian-michel@gmx.de>
2025-03-05 11:59:17 +0100

    working on adding quickdraws

--- physics.js
+++ physics.js
@@ -1,72 +1,272 @@
 
 class Rope {
   // default: one fixed end at 0, 70kg hanging at the other end (straight down)
-  constructor(length = 5, segments = 1, end1 = new Body(0, 0, 0, 0), end2 = new Body(0, -length, 0, 70)) {
+  constructor(length = 5, segments = 1, end1 = new Body(0, 0, 0, 0), end2 = new Body(0, -length, 0, 70), ...deflectionPoints) {
+    this.significantMassAtEnd2 = (end2.mass > 10 * this.segmentMass);
     this.restLength = length;
     this.mass = length * 0.062; // 62 g per meter of rope weight of typical climbing rope
     this.elasticityConstant = 0.079e-3; // 0.079e-3 1/N elasticity constant of typical climbing rope
     this.segmentLength = length / segments;
     this.segmentSpringConstant = 1 / (this.segmentLength * this.elasticityConstant);
-    this.dampingCoefficient = 0.1 / this.segmentLength; // damping for oscillations orthogonal to the rope
+    this.dampingCoefficient = 0.02 / this.segmentLength; // damping for oscillations orthogonal to the rope
     this.internalDamping = 0.01 / this.segmentLength; // damping for internal friction
     this.segmentMass = (segments > 1) ? this.mass / (segments - 1) : 0; // ends have their own predefined masses
+    this.ropeSegments = [];
     this.bodies = [end1];
     for (let i = 1; i < segments; i++) {
       this.bodies.push(new Body(...end1.pos.plus(end2.pos.minus(end1.pos).times(i / segments)).arr, this.segmentMass));
-      this.bodies[i].velocityDamping = 0.6;
+      // if (this.significantMassAtEnd2)
+      //   this.bodies[i].velocityDamping = 0.6;
+      this.ropeSegments.push(new RopeSegment(this.bodies[i-1], this.bodies[i],
+        this.segmentLength, this.segmentSpringConstant, this.dampingCoefficient, this.internalDamping));
     }
     this.bodies.push(end2);
-    this.currentLength = end2.pos.minus(end1.pos).norm();
+    this.ropeSegments.push(new RopeSegment(this.bodies[this.bodies.length-2], this.bodies[this.bodies.length-1],
+      this.segmentLength, this.segmentSpringConstant, this.dampingCoefficient, this.internalDamping));
+    for (let i = 0; i < this.ropeSegments.length; i++) {
+      if (i > 0) this.ropeSegments[i].previousSegment = this.ropeSegments[i-1];
+      if (i+1 < this.ropeSegments.length) this.ropeSegments[i].followingSegment = this.ropeSegments[i+1];
+    }
+    if (deflectionPoints.length > 0) {
+      const deflPts = [end1, ...deflectionPoints, end2];
+      let cumLen = 0;
+      const lenArr = [];
+      for (let i = 1; i < deflPts.length; i++) {
+        const len = deflPts[i].pos.minus(deflPts[i-1].pos).norm();
+        cumLen += len;
+        lenArr.push(cumLen);
+      }
+      this.currentLength = cumLen;
+      let lenArrIdx = 0;
+      for (let i = 1; i+1 < this.bodies.length; i++) {
+        const partialRopeLen = i * (this.currentLength / segments);
+        while (lenArrIdx < lenArr.length && partialRopeLen > lenArr[lenArrIdx]) {
+          this.ropeSegments[i-1].deflectionPoints.push(deflectionPoints[lenArrIdx]);
+          lenArrIdx++;
+        }
+        const dSegLen = lenArr[lenArrIdx] - (lenArrIdx == 0 ? 0 : lenArr[lenArrIdx-1]);
+        const dSegFac = (partialRopeLen - (lenArrIdx == 0 ? 0 : lenArr[lenArrIdx-1])) / dSegLen;
+        this.bodies[i].pos = deflPts[lenArrIdx].pos.times(1 - dSegFac).plus(deflPts[lenArrIdx+1].pos.times(dSegFac));
+      }
+      while (lenArrIdx < deflectionPoints.length) {
+        this.ropeSegments[this.ropeSegments.length - 1].push(deflectionPoints[lenArrIdx]);
+        lenArrIdx++;
+      }
+    } else {
+      this.currentLength = end2.pos.minus(end1.pos).norm();
+    }
     this.currentStretchingForce = (this.currentLength - this.restLength) / (this.restLength * this.elasticityConstant);
     this.maxStretchingForce = this.currentStretchingForce;
     this.maxEndSpeed = end2.velocity.norm();
   }
 
   applyGravity(f) {
-    for (let i = 0; i < this.bodies.length; i++) {
-      this.bodies[i].applyGravity(f);
+    for (let i = 0; i < this.ropeSegments.length; i++) {
+      this.ropeSegments[i].applyGravity(f);
     }
   }
 
   applyRopeForces() {
     this.currentLength = 0;
-    for (let i = 1; i < this.bodies.length; i++) {
-      const bA = this.bodies[i-1];
-      const bB = this.bodies[i];
-      const diff = bB.pos.minus(bA.pos);
-      const len = diff.norm();
-      this.currentLength += len;
-      if (len > 0) {
-        const direction = diff.times(1 / len);
-        const springForceMag = (len - this.segmentLength) * this.segmentSpringConstant;
-        const springForce = direction.times(springForceMag);
-        bA.applyForce(springForce);
-        bB.applyForce(springForce.times(-1));
-
-        const relativeVelocity = bB.velocity.minus(bA.velocity);
-        const lengthChangeRate = relativeVelocity.dot(direction);
-        if (bA.mass > 0 && bB.mass > 0) {
-          const relativeParallel = direction.times(lengthChangeRate);
-          const relativePerp = relativeVelocity.minus(relativeParallel);
-          const dampingForce = relativePerp.times(this.dampingCoefficient);
-          bA.applyForce(dampingForce);
-          bB.applyForce(dampingForce.times(-1));
-        }
-        
-        const internalDampingForce = direction.times(lengthChangeRate * this.internalDamping);
-        bA.applyForce(internalDampingForce);
-        bB.applyForce(internalDampingForce.times(-1));
-      }
+    for (let i = 0; i < this.ropeSegments.length; i++) {
+      this.ropeSegments[i].applyRopeForces();
+      this.currentLength += this.ropeSegments[i].currentLength;
     }
     this.currentStretchingForce = (this.currentLength - this.restLength) / (this.restLength * this.elasticityConstant);
     this.maxStretchingForce = Math.max(this.currentStretchingForce, this.maxStretchingForce);
   }
 
-  timeStep(delta, clearForces = true) {
-    for (let i = 1; i < this.bodies.length; i++) {
-      this.bodies[i].timeStep(delta, clearForces);
+  timeStep(delta, clearForces = true, debug = false) {
+    for (let i = 0; i < this.ropeSegments.length; i++) {
+      this.ropeSegments[i].timeStep(delta, clearForces, debug);
     }
     this.maxEndSpeed = Math.max(this.maxEndSpeed, this.bodies[this.bodies.length - 1].velocity.norm());
+  }
+}
+
+class RopeSegment {
+  constructor(end1, end2, restLength, springConstant, dampingCoefficient, internalDamping) {
+    this.bodyA = end1;
+    this.bodyB = end2;
+    this.currentLength = this.bodyB.pos.minus(this.bodyA.pos).norm();
+    this.restLength = restLength;
+    this.springConstant = springConstant;
+    this.dampingCoefficient = dampingCoefficient;
+    this.internalDamping = internalDamping;
+    this.previousSegment = null; // 2nd rope segment attached to bodyA (null if bodyA is the end of the rope)
+    this.followingSegment = null; // 2nd rope segment attached to bodyB (null if bodyB is the end of the rope)
+    this.deflectionPoints = []; // simulates that the rope passes through carabiners
+  }
+
+  applyGravity(f) {
+    this.bodyA.applyGravity(f);
+    if (this.followingSegment === null)
+      this.bodyB.applyGravity(f);
+  }
+
+  applyRopeForces() {
+    this.currentLength = 0;
+    let len = 0;
+    this.tmpDiffArr = [];
+    this.tmpLenArr = [];
+    const deflPts = [this.bodyA, ...this.deflectionPoints, this.bodyB];
+    let startDiff = null; let startDiffLen = 0;
+    let endDiff = null; let endDiffLen = 0;
+    this.tmpPosA = this.bodyA.pos;
+    for (let i = 1; i < deflPts.length; i++) {
+      const bodyA = deflPts[i-1];
+      const bodyB = deflPts[i];
+      const diff = bodyB.pos.minus(bodyA.pos);
+      const diffLen = diff.norm();
+      len += diffLen;
+      this.tmpDiffArr.push(diff);
+      this.tmpLenArr.push(diffLen);
+      if (diffLen > 0) {
+        if (startDiffLen == 0) {
+          startDiff = diff;
+          startDiffLen = diffLen;
+        }
+        endDiff = diff;
+        endDiffLen = diffLen;
+      }
+    }
+    this.tmpStartDiff = startDiff;
+    this.tmpStartDiffLen = startDiffLen;
+    this.tmpEndDiff = endDiff;
+    this.tmpEndDiffLen = endDiffLen;
+    this.tmpDeflectionPoints = [...this.deflectionPoints];
+    this.currentLength = len;
+
+    const diffTot = this.bodyB.pos.minus(this.bodyA.pos);
+    const lenTot = diffTot.norm();
+    if (len > 0) {
+      const directionA = startDiff.times(1 / startDiffLen);
+      const directionB = endDiff.times(1 / endDiffLen);
+      this.tmpDirectionA = directionA;
+      this.tmpDirectionB = directionB;
+      const springForceMag = (len - this.restLength) * this.springConstant;
+      this.bodyA.applyForce(directionA.times(springForceMag));
+      this.bodyB.applyForce(directionB.times(-springForceMag));
+
+      if (lenTot > 0 && this.deflectionPoints.length == 0) { // TODO: damping if deflection points present
+        const direction = diffTot.times(1 / lenTot);
+        const relativeVelocity = this.bodyB.velocity.minus(this.bodyA.velocity);
+        const lengthChangeRate = relativeVelocity.dot(direction);
+        
+        if (this.bodyA.mass > 0 && this.bodyB.mass > 0) {
+          const relativeParallel = direction.times(lengthChangeRate);
+          const relativePerp = relativeVelocity.minus(relativeParallel);
+          const dampingForce = relativePerp.times(this.dampingCoefficient);
+          this.bodyA.applyForce(dampingForce);
+          this.bodyB.applyForce(dampingForce.times(-1));
+        }
+      
+        this.bodyA.applyForce(directionA.times(lengthChangeRate * this.internalDamping));
+        this.bodyB.applyForce(directionB.times(-lengthChangeRate * this.internalDamping));
+      }
+    }
+  }
+  
+  forwardDeflectionPointHelper(delta_pos, delta_fac) { // bodyA moves towards first deflection point of this rope segment
+    // here, we don't need this.tmpDeflectionPoints as the deflection points of this segment cannot have changed yet
+    if (delta_fac <= this.tmpLenArr[0]) { // bodyA does not pass through the deflection point
+      this.bodyA.pos = this.bodyA.pos.plus(delta_pos);
+    } else { // bodyA passes through the deflection point
+      const additionalFreeMovement = delta_pos.minus(this.tmpDirectionA.times(delta_fac));
+      let csum = 0;
+      let didx = 0;
+      while (delta_fac > csum && didx < this.tmpLenArr.length) {
+        csum += this.tmpLenArr[didx];
+        didx++;
+      }
+      didx--;
+      const prevLen = csum - this.tmpLenArr[didx];
+      const currLen = this.tmpLenArr[didx];
+      const deflPt2Weight = (delta_fac - prevLen) / currLen;
+      if (didx+1 < this.tmpLenArr.length) { // bodyA is now exactly between two deflection points
+        this.bodyA.pos = this.deflectionPoints[didx-1].pos.times(1 - deflPt2Weight).plus(this.deflectionPoints[didx].pos.times(deflPt2Weight)).plus(additionalFreeMovement);
+        for (let i = 0; i < didx; i++) {
+          const dPoint = this.deflectionPoints.shift();
+          if (this.previousSegment !== null)
+            this.previousSegment.deflectionPoints.push(dPoint);
+        }
+      } else { // bodyA passed through all deflection points
+        this.bodyA.pos = this.deflectionPoints[didx-1].pos.plus(this.tmpDirectionB.times(delta_fac - prevLen)).plus(additionalFreeMovement);
+        if (this.previousSegment !== null) {
+          for (let i = 0; i < this.deflectionPoints.length; i++)
+            this.previousSegment.deflectionPoints.push(this.deflectionPoints[i]);
+        }
+        this.deflectionPoints = [];
+      }
+    }
+  }
+
+  backwardDeflectionPointHelper(delta_pos, delta_fac) { // bodyB moves towards last deflection point of this rope segment
+    // here, we need this.tmpDeflectionPoints as the deflection points of this segment might already have changed if bodyA moved over deflection points
+    if (-delta_fac <= this.tmpLenArr[this.tmpLenArr.length - 1]) { // bodyB does not pass through the last deflection point
+      this.bodyB.pos = this.bodyB.pos.plus(delta_pos);
+    } else { // bodyB passes through the last deflection point
+      const additionalFreeMovement = delta_pos.minus(this.tmpDirectionB.times(delta_fac));
+      let csum = 0;
+      let didx = this.tmpLenArr.length - 1;
+      while (-delta_fac > csum && didx >= 0) {
+        csum += this.tmpLenArr[didx];
+        didx--;
+      }
+      didx++;
+      const prevLen = csum - this.tmpLenArr[didx];
+      const currLen = this.tmpLenArr[didx];
+      const deflPt2Weight = (-delta_fac - prevLen) / currLen;
+      if (didx > 0) { // bodyB is now exactly between two deflection points
+        this.bodyB.pos = this.tmpDeflectionPoints[didx].pos.times(1 - deflPt2Weight).plus(this.tmpDeflectionPoints[didx-1].pos.times(deflPt2Weight)).plus(additionalFreeMovement);
+        for (let i = 0; i < this.tmpLenArr.length - 1 - didx; i++) {
+          const dPoint = this.deflectionPoints.pop();
+          if (this.followingSegment !== null)
+            this.followingSegment.deflectionPoints.unshift(dPoint);
+        }
+      } else { // bodyB passed through all deflection points
+        this.bodyB.pos = this.tmpDeflectionPoints[0].pos.plus(this.tmpDirectionA.times(delta_fac + prevLen)).plus(additionalFreeMovement);
+        for (let i = 0; i < this.tmpDeflectionPoints.length; i++) {
+          const dPoint = this.deflectionPoints.pop();
+          if (this.followingSegment !== null)
+            this.followingSegment.deflectionPoints.unshift(dPoint);
+        }
+      }
+    }
+  }
+
+  timeStep(delta, clearForces = true) {
+    if (this.deflectionPoints.length == 0 && (this.previousSegment === null || this.previousSegment.tmpDeflectionPoints.length == 0)) {
+      this.bodyA.timeStep(delta, clearForces);
+    } else {
+      const delta_pos = this.bodyA.timeStep(delta, clearForces, false);
+      const delta_fac = this.deflectionPoints.length > 0 ? this.tmpDirectionA.dot(delta_pos) : 0;
+      if (delta_fac > 0) { // bodyA moves towards first deflection point of this rope segment
+        this.forwardDeflectionPointHelper(delta_pos, delta_fac);
+      } else if (this.previousSegment !== null && this.previousSegment.tmpDeflectionPoints.length > 0) {
+        const delta2_fac = this.previousSegment.tmpDirectionB.dot(delta_pos);
+        if (delta2_fac < 0) { // bodyA moves towards last deflection point of previous rope segment
+          this.previousSegment.backwardDeflectionPointHelper(delta_pos, delta2_fac);
+        } else {
+          this.bodyA.pos = this.bodyA.pos.plus(delta_pos);
+        }
+      } else {
+        this.bodyA.pos = this.bodyA.pos.plus(delta_pos);
+      }
+    }
+    if (this.followingSegment === null) {
+      if (this.tmpDeflectionPoints.length == 0) {
+        this.bodyB.timeStep(delta, clearForces);
+      } else {
+        const delta_pos = this.bodyB.timeStep(delta, clearForces, false);
+        const delta_fac = this.tmpDirectionB.dot(delta_pos);
+        if (delta_fac < 0) { // bodyB moves towards last deflection point of this rope segment
+          this.backwardDeflectionPointHelper(delta_pos, delta_fac);
+        } else {
+          this.bodyB.pos = this.bodyB.pos.plus(delta_pos);
+        }
+      }
+    }
   }
 }
 
@@ -92,12 +292,15 @@ class Body {
     this.appliedForces = this.appliedForces.plus(f.times(this.mass));
   }
 
-  timeStep(delta, clearForces = true) {
+  timeStep(delta, clearForces = true, applyChanges = true) {
     if (this.mass > 0) {
       const acceleration = this.appliedForces.times(1 / this.mass);
       this.velocity = this.velocity.plus(acceleration.times(delta)).times(Math.pow(this.velocityDamping, delta));
-      this.pos = this.pos.plus(this.velocity.times(delta));
+      if (applyChanges)
+        this.pos = this.pos.plus(this.velocity.times(delta));
     }
     if (clearForces) this.clearForces();
+    if (!applyChanges)
+      return this.velocity.times(delta);
   }
 }
--- script.js
+++ script.js
@@ -14,14 +14,24 @@ const GLOBALS = {
 };
 
 function main() {
-  GLOBALS.startHeight = 3;
-  GLOBALS.ropeLength = 5;
+  GLOBALS.startHeight = 3; // height above last draw
+  GLOBALS.lastDrawHeight = 5; // height of last draw
+  GLOBALS.ropeLength = GLOBALS.startHeight + GLOBALS.lastDrawHeight + 0.1;
+  GLOBALS.ropeSegmentNum = 30;
   GLOBALS.freeFall = GLOBALS.startHeight + GLOBALS.ropeLength;
   GLOBALS.fallFactor = GLOBALS.freeFall / GLOBALS.ropeLength;
-  GLOBALS.climber = new Body(0.01 + Math.sqrt(GLOBALS.ropeLength * GLOBALS.ropeLength - GLOBALS.startHeight * GLOBALS.startHeight), GLOBALS.startHeight, 0, 70);
-  GLOBALS.climber.velocityDamping = 0.95;
+  GLOBALS.anchor = new Body(0, -GLOBALS.lastDrawHeight, 0, 0);
+  GLOBALS.deflectionPoint = new Body(0.005, 0, 0, 0);
+  GLOBALS.anchorDPointLen = GLOBALS.deflectionPoint.pos.minus(GLOBALS.anchor.pos).norm();
+  GLOBALS.dPointClimLen = GLOBALS.ropeLength - GLOBALS.anchorDPointLen;
+  GLOBALS.climber = new Body(0.01 + Math.sqrt(GLOBALS.dPointClimLen * GLOBALS.dPointClimLen - GLOBALS.startHeight * GLOBALS.startHeight), GLOBALS.startHeight, 0, 70);
+  GLOBALS.climber.velocityDamping = 0.95; // TODO: improve
   GLOBALS.climber.velocity = new V(0, 0, 0);
-  GLOBALS.rope = new Rope(GLOBALS.ropeLength, 30, new Body(0, 0, 0, 0), GLOBALS.climber);
+  GLOBALS.climber.mass = (GLOBALS.ropeLength * 0.062) / (GLOBALS.ropeSegmentNum - 1); // no climber at the end of the rope
+  if (GLOBALS.lastDrawHeight == 0)
+    GLOBALS.rope = new Rope(GLOBALS.ropeLength, GLOBALS.ropeSegmentNum, GLOBALS.anchor, GLOBALS.climber);
+  else
+    GLOBALS.rope = new Rope(GLOBALS.ropeLength, GLOBALS.ropeSegmentNum, GLOBALS.anchor, GLOBALS.climber, GLOBALS.deflectionPoint);
   GLOBALS.expectedForce = GLOBALS.climber.mass * GRAVITY_OF_EARTH
     + Math.sqrt(GLOBALS.climber.mass * GRAVITY_OF_EARTH * GLOBALS.climber.mass * GRAVITY_OF_EARTH
                 + 2 * GLOBALS.climber.mass * GRAVITY_OF_EARTH * GLOBALS.fallFactor / GLOBALS.rope.elasticityConstant);
@@ -29,9 +39,28 @@ function main() {
   const FPS = 40;
   const snapshots = precalculatePositions(25, FPS);
   GLOBALS.startingTime = (new Date()).getTime() / 1000;
+  // framePerFrame(snapshots);
+
   window.requestAnimationFrame(() => playInLoop(snapshots, FPS));
+
   // GLOBALS.lastTimeStep = (new Date()).getTime() / 1000;
   // window.requestAnimationFrame(mainSimulationLoop);
+}
+
+function framePerFrame(snapshots) {
+  let cIdx = 0;
+  drawRope(snapshots[cIdx]);
+
+  window.addEventListener('click', () => {
+    cIdx++;
+    cIdx %= snapshots.length;
+    drawRope(snapshots[cIdx]);
+  });
+  window.addEventListener('keydown', () => {
+    cIdx++;
+    cIdx %= snapshots.length;
+    drawRope(snapshots[cIdx]);
+  });
 }
 
 function playInLoop(snapshots, FPS) {
@@ -46,21 +75,33 @@ function precalculatePositions(targetTime, FPS = 40) {
   const snapshots = [];
   const addSnapshot = (t) => {
     const ss = [];
-    for (let i = 0; i < GLOBALS.rope.bodies.length; i++)
+    const ss_defl = [];
+    const ss_forc = [];
+    for (let i = 0; i < GLOBALS.rope.bodies.length; i++) {
       ss.push(GLOBALS.rope.bodies[i].pos);
+      ss_forc.push(GLOBALS.rope.bodies[i].appliedForces);
+      ss_defl.push([]);
+      if (i+1 < GLOBALS.rope.bodies.length) {
+        for (let k = 0; k < GLOBALS.rope.ropeSegments[i].deflectionPoints.length; k++)
+          ss_defl[i].push(GLOBALS.rope.ropeSegments[i].deflectionPoints[k].pos);
+      }
+    }
     snapshots.push({
-      positions: ss, cTime: t, timeDelay: 0, maxStretchingForce: GLOBALS.rope.maxStretchingForce,
+      positions: ss, deflectionPoints: ss_defl, cTime: t, timeDelay: 0, maxStretchingForce: GLOBALS.rope.maxStretchingForce,
       expectedForce: GLOBALS.expectedForce, climberGravity: GLOBALS.climber.mass * GRAVITY_OF_EARTH,
-      maxSpeed: GLOBALS.rope.maxEndSpeed
+      maxSpeed: GLOBALS.rope.maxEndSpeed,
+      forces: ss_forc
      });
   };
+  GLOBALS.rope.applyGravity(GRAVITY_VEC);
+  GLOBALS.rope.applyRopeForces();
   addSnapshot(0);
   const numSteps = targetTime / GLOBALS.maxStep;
   let lastSnapshot = 0;
   for (let i = 1; i <= numSteps; i++) {
+    GLOBALS.rope.timeStep(GLOBALS.maxStep);
     GLOBALS.rope.applyGravity(GRAVITY_VEC);
     GLOBALS.rope.applyRopeForces();
-    GLOBALS.rope.timeStep(GLOBALS.maxStep);
     if (i * GLOBALS.maxStep - lastSnapshot >= 1 / FPS) {
       addSnapshot(i * GLOBALS.maxStep);
       lastSnapshot = i * GLOBALS.maxStep;
@@ -70,7 +111,7 @@ function precalculatePositions(targetTime, FPS = 40) {
 }
 
 function drawRope(infoObj) {
-  const { positions, cTime, timeDelay, maxStretchingForce, expectedForce, climberGravity, maxSpeed } = infoObj;
+  const { positions, deflectionPoints, forces, cTime, timeDelay, maxStretchingForce, expectedForce, climberGravity, maxSpeed } = infoObj;
 
   const canvas = document.getElementById('main-canvas');
   if (canvas.width != GLOBALS.canvasWidth || canvas.height !=  GLOBALS.canvasHeight) {
@@ -116,40 +157,76 @@ function drawRope(infoObj) {
   ctx.fillText(`max. speed of climber: ${numToStr(maxSpeed * 3.6, 2, 11)} km/h`, GLOBALS.canvasWidth - 5, GLOBALS.canvasHeight / 2 + 100);
 
   ctx.beginPath();
+  const dPos = GLOBALS.deflectionPoint.pos;
+  ctx.arc(XORIG + dPos.x*SCALE, YORIG - dPos.y*SCALE, 3, 0, Math.PI * 2);
+  ctx.fillStyle = 'blue';
+  ctx.fill();
+  ctx.closePath();
+
+  ctx.beginPath();
   const fPos = positions[0];
-  ctx.arc(XORIG + fPos.x*SCALE, YORIG - fPos.y*SCALE, 4, 0, Math.PI * 2);
+  ctx.arc(XORIG + fPos.x*SCALE, YORIG - fPos.y*SCALE, 3, 0, Math.PI * 2);
   ctx.fillStyle = 'red';
   ctx.fill();
   ctx.closePath();
 
   ctx.beginPath();
   const hPos = positions[positions.length - 1];
-  ctx.arc(XORIG + hPos.x*SCALE, YORIG - hPos.y*SCALE, 4, 0, Math.PI * 2);
+  ctx.arc(XORIG + hPos.x*SCALE, YORIG - hPos.y*SCALE, 3, 0, Math.PI * 2);
   ctx.fillStyle = 'green';
   ctx.fill();
   ctx.closePath();
 
-  // for (let i = 1; i < positions.length - 1; i++) {
+  for (let i = 1; i < positions.length - 1; i++) {
+    const segPos = positions[i];
+    ctx.beginPath();
+    ctx.arc(XORIG + segPos.x*SCALE, YORIG - segPos.y*SCALE, 4, 0, Math.PI * 2);
+    ctx.strokeStyle = 'gray';
+    ctx.lineWidth = 1;
+    ctx.stroke();
+    ctx.closePath();
+  }
+
+  let started = false;
+  for (let i = 0; i < positions.length; i++) {
+    const segPos = positions[i];
+    if (!started) {
+      ctx.beginPath();
+      ctx.moveTo(XORIG + segPos.x*SCALE, YORIG - segPos.y*SCALE);
+    } else {
+      ctx.lineTo(XORIG + segPos.x*SCALE, YORIG - segPos.y*SCALE);
+    }
+    started = true;
+    const deflPos = deflectionPoints[i];
+    for (let k = 0; k < deflPos.length; k++)
+      ctx.lineTo(XORIG + deflPos[k].x*SCALE, YORIG - deflPos[k].y*SCALE);
+    if (deflPos.length > 0) {
+      ctx.strokeStyle = 'black';
+      ctx.lineWidth = 2;
+      ctx.stroke();
+      ctx.closePath();
+      ctx.beginPath();
+      ctx.moveTo(XORIG + deflPos[deflPos.length-1].x*SCALE, YORIG - deflPos[deflPos.length-1].y*SCALE);
+    }
+  }
+  ctx.strokeStyle = 'rgb(60,60,60)';
+  ctx.lineWidth = 2;
+  ctx.stroke();
+  ctx.closePath();
+  
+  // const FORCE_SCALE = 5;
+  // for (let i = 0; i < positions.length; i++) {
   //   const segPos = positions[i];
+  //   const force = forces[i];
   //   ctx.beginPath();
-  //   ctx.arc(XORIG + segPos.x*SCALE, YORIG - segPos.y*SCALE, 4, 0, Math.PI * 2);
-  //   ctx.strokeStyle = 'gray';
+  //   ctx.moveTo(XORIG + segPos.x*SCALE, YORIG - segPos.y*SCALE);
+  //   ctx.lineTo(XORIG + (segPos.x + force.x*FORCE_SCALE)*SCALE, YORIG - (segPos.y + force.y*FORCE_SCALE)*SCALE);
+  //   ctx.strokeStyle = `rgb(${Math.round((i / (positions.length-1))*255)},123,123)`;
   //   ctx.lineWidth = 1;
   //   ctx.stroke();
   //   ctx.closePath();
   // }
 
-  ctx.beginPath();
-  const topPos = positions[0];
-  ctx.moveTo(XORIG + topPos.x*SCALE, YORIG - topPos.y*SCALE);
-  for (let i = 1; i < positions.length; i++) {
-    const segPos = positions[i];
-    ctx.lineTo(XORIG + segPos.x*SCALE, YORIG - segPos.y*SCALE);
-  }
-  ctx.strokeStyle = 'black';
-  ctx.lineWidth = 2;
-  ctx.stroke();
-  ctx.closePath();
 }
 
 function mainSimulationLoop() {
@@ -173,7 +250,9 @@ function mainSimulationLoop() {
   drawRope({
     positions: GLOBALS.rope.bodies.map(item => item.pos), cTime: GLOBALS.cTime, timeDelay: GLOBALS.timeDelay, maxStretchingForce: GLOBALS.rope.maxStretchingForce,
     expectedForce: GLOBALS.expectedForce, climberGravity: GLOBALS.climber.mass * GRAVITY_OF_EARTH,
-    maxSpeed: GLOBALS.rope.maxEndSpeed
+    maxSpeed: GLOBALS.rope.maxEndSpeed,
+    deflectionPoints: GLOBALS.rope.bodies.map(item => []), // TODO!
+    forces: GLOBALS.rope.bodies.map(item => item.appliedForces)
    });
   
   GLOBALS.lastTimeStep = cTime;
